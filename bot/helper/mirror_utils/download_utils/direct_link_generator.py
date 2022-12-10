9fed83dfb4798b1a4fb0a13da56e8d3d1e116e61
Kaam Dev <97149710+KaamDev@users.noreply.github.com>
2022-12-10 19:19:27 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -74,6 +74,8 @@ def direct_link_generator(link: str):
         return uploadee(link)
     elif 'terabox' in domain:
         return terabox(link)
+    elif 'gdtot' in domain:
+        return gdtot(link)
     elif any(x in domain for x in fmed_list):
         return fembed(link)
     elif any(x in domain for x in ['sbembed.com', 'watchsb.com', 'streamsb.net', 'sbplay.org']):
@@ -440,3 +442,22 @@ def terabox(url) -> str:
     if result['isdir'] != '0':
         raise DirectDownloadLinkException("ERROR: Can't download folder")
     return result['dlink']
+  
+  def gdtot(url: str) -> str:
+    """ Gdtot google drive link generator
+    By https://github.com/xcscxr """
+    
+    parsed_url = urlparse(url)
+    client = rsession()
+    try:
+        client.cookies.set(name='crypt', value="UllSWmE0YjZ3UVNRc0NMZ3JqS3c2R0JOUS9FWGVreG1GZkVhUkRrNElWVT0%3D", domain=parsed_url.netloc)
+        res = client.get(url)
+    except Exception as e:
+        raise DirectDownloadLinkException(f"ERROR: {e}")
+    try:
+        res = client.get(f"{parsed_url.scheme}://{parsed_url.netloc}/dld?id={url.split('/')[-1]}")
+        matches = re_findall('gd=(.*?)&', res.text)
+        decoded_id = b64decode(str(matches[0])).decode('utf-8')
+        return f'https://drive.google.com/open?id={decoded_id}'
+    except:
+        raise DirectDownloadLinkException("ERROR: Try in your browser, mostly file not found or user limit exceeded!")
